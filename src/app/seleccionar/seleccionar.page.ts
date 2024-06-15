4a7d09c9c4b05076a72d916872d410f234cf559b
Patricio Quintanilla <patricioquintanilladx@gmail.com>
2024-06-15 08:45:11 -0400

    edits

--- src/app/seleccionar/seleccionar.page.ts
+++ src/app/seleccionar/seleccionar.page.ts
@@ -1,21 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import { CategoriaService } from '../services/api.service';
+// import { Component, OnInit } from '@angular/core';
+// import { CategoriaService } from '../services/api.service';
 
+
+// @Component({
+//   selector: 'app-seleccionar',
+//   templateUrl: './seleccionar.page.html',
+//   styleUrls: ['./seleccionar.page.scss'],
+// })
+// export class SeleccionarPage implements OnInit {
+//   categories: any;
+
+//   constructor(private categoriaService: CategoriaService) {}
+
+//   ngOnInit() {
+//     this.categoriaService.getCategorias().subscribe((data: any) => {
+//       this.categories = data.categories;
+//     });
+//   }
+// }
+
+import { Component, ViewChild } from '@angular/core';
+import { IonContent } from '@ionic/angular';
+import { CategoriaService } from '../services/api.service';
 
 @Component({
   selector: 'app-seleccionar',
   templateUrl: './seleccionar.page.html',
   styleUrls: ['./seleccionar.page.scss'],
 })
-export class SeleccionarPage implements OnInit {
-  categories: any;
+export class SeleccionarPage {
+  @ViewChild(IonContent, { static: false }) content: IonContent;
 
   constructor(private categoriaService: CategoriaService) {}
 
+  scrollToTop() {
+    this.content.scrollToTop(400); // 400ms de duración de la animación
+  }
   ngOnInit() {
     this.categoriaService.getCategorias().subscribe((data: any) => {
       this.categories = data.categories;
     });
   }
 }
+
 
